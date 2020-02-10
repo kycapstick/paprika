f989eda6d5be4d38f8abae0f2d7de4a1122db7b0
kweenofcode <ky.capstick@gmail.com>
2020-02-09 19:31:00 -0500

    Removing unneccessary stylesheet code

--- wp-content/themes/paprika-2020/includes/theme-scripts.php
+++ wp-content/themes/paprika-2020/includes/theme-scripts.php
@@ -1,7 +1,7 @@
 <?php 
   if (!function_exists('add_theme_scripts')):
     function add_theme_scripts() {
-      wp_enqueue_style('main', get_template_directory_uri() . '/dist/main.css', array(), filemtime(get_template_directory() . '/dist/main.css'), false );
+      wp_enqueue_style('main', get_template_directory_uri() . '/dist/main.css', array());
       wp_enqueue_script('main_scripts', get_template_directory_uri() . '/dist/main.js', array('jquery'), true);
     }
   endif;
