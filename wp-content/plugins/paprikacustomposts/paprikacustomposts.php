634a1790d8387d17201984f50a43cbb5a63cc874
kweenofcode <ky.capstick@gmail.com>
2020-05-18 19:33:20 -0400

    Moving custom functionality to plugin

---
+++ wp-content/plugins/paprikacustomposts/paprikacustomposts.php
@@ -0,0 +1,21 @@
+<?php
+    /*
+    Plugin Name: Paprika Custom Posts
+    Plugin URI: 
+    description: >-
+    a plugin to create the custom post types for the Paprika festival website
+    Version: 1.0
+    Author: Ky Capstick
+    Author URI: https://github.com/kycapstick
+    */
+
+    require_once( __DIR__ . '/includes/theme-scripts.php');
+
+    require_once( __DIR__ . '/includes/utilities.php');
+
+    require_once( __DIR__ . '/includes/custom-post-types.php');
+
+    require_once( __DIR__ . '/includes/metaboxes/index.php');
+
+    require_once( __DIR__ . '/includes/database/database.php');
+?>
--- wp-content/themes/paprika-2020/functions.php
+++ wp-content/themes/paprika-2020/functions.php
@@ -1,14 +1,4 @@
 <?php 
-  require_once( __DIR__ . '/includes/theme-scripts.php');
-
-  require_once( __DIR__ . '/includes/utilities.php');
-
-  require_once( __DIR__ . '/includes/custom-post-types.php');
-
-  require_once( __DIR__ . '/includes/metaboxes/index.php');
-
-  require_once( __DIR__ . '/includes/database/database.php');
-
 
 function paprika_head_metadata() {
 
@@ -20,6 +10,3 @@ function paprika_head_metadata() {
 
 }
 add_action( 'wp_head', 'paprika_head_metadata' );
-
-
-
