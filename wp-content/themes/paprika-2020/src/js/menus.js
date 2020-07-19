827bb413cc3ad51f53087ed05b0e3e84aaaec57b
kweenofcode <ky.capstick@gmail.com>
2020-07-19 15:36:09 -0400

    Adding keyboard navigation to header menu

--- wp-content/themes/paprika-2020/src/js/menus.js
+++ wp-content/themes/paprika-2020/src/js/menus.js
@@ -1,19 +1,72 @@
+const addTrapFocusListeners = (first, last) => {
+	first.addEventListener("keydown", (e) => {
+		if (e.keyCode === 9 && e.shiftKey) {
+			e.preventDefault();
+			last.focus();
+		}
+	});
+	last.addEventListener("keydown", (e) => {
+		if (e.keyCode === 9 && !e.shiftKey) {
+			e.preventDefault();
+			first.focus();
+		}
+	});
+};
+
+const trapFocus = (menu, firstElement) => {
+	const activeItems = [
+		...menu.querySelectorAll('button, a, [tabindex]:not([tabindex="-1"])'),
+	];
+	const lastElement = activeItems.pop();
+	addTrapFocusListeners(firstElement, lastElement);
+};
+
+const addFocusTrapping = () => {
+	const headerMenu = document.querySelector(".header-menu");
+	const firstElement = document.querySelector(".header__trigger");
+	trapFocus(headerMenu, firstElement);
+	allowEscKey(headerMenu);
+};
+
 // MAIN MENU
 const activateMenu = () => {
 	const menu = document.querySelector(".header-menu");
 	menu.classList.toggle("header-menu--active");
+	document.body.classList.toggle("noscroll");
+};
+
+const allowEscKey = () => {
+	const headerMenu = document.querySelector(".header-menu");
+	const menuTrigger = document.querySelector(".header__trigger");
+	headerMenu.addEventListener("keyup", (e) => {
+		if (!headerMenu.classList.contains("header-menu--active")) {
+			return;
+		}
+		if (e.keyCode === 27) {
+			menuTrigger.classList.toggle("header__trigger--active");
+			activateMenu();
+		}
+	});
 };
 
 const handleMenuTriggerClick = () => {
 	const menuTrigger = document.querySelector(".header__trigger");
 	menuTrigger.addEventListener("click", (e) => {
-		activateMenu();
-		document.body.classList.toggle("noscroll");
 		if (e.target.classList.contains("header__trigger__bar")) {
 			e.target.parentNode.classList.toggle("header__trigger--active");
 			return;
 		}
 		e.target.classList.toggle("header__trigger--active");
+		activateMenu();
+	});
+	menuTrigger.addEventListener("keyup", (e) => {
+		if (!e.target.classList.contains("header__trigger--active")) {
+			return;
+		}
+		if (e.keyCode === 27) {
+			e.target.classList.toggle("header__trigger--active");
+			activateMenu();
+		}
 	});
 };
 
@@ -24,6 +77,16 @@ const clearActiveMenu = () => {
 	}
 };
 
+const activateSubmenu = (item) => {
+	const target = item.tagName === "A" ? item.parentNode : item;
+	if (target.classList.contains("menu-item--active")) {
+		target.classList.remove("menu-item--active");
+		return;
+	}
+	clearActiveMenu();
+	target.classList.add("menu-item--active");
+};
+
 const handleTopLevelMenuItemClick = () => {
 	const topLevelMenuItems = document.querySelectorAll(
 		"#menu-main > .menu-item-has-children > a"
@@ -32,14 +95,12 @@ const handleTopLevelMenuItemClick = () => {
 		topLevelMenuItems.forEach((menuItem) => {
 			menuItem.addEventListener("click", (e) => {
 				e.preventDefault();
-				const target =
-					e.target.tagName === "A" ? e.target.parentNode : e.target;
-				if (target.classList.contains("menu-item--active")) {
-					target.classList.remove("menu-item--active");
-					return;
+				activateSubmenu(e.target);
+			});
+			menuItem.addEventListener("keypress", (e) => {
+				if (e.keyCode === 32) {
+					activateSubmenu(e.target);
 				}
-				clearActiveMenu();
-				target.classList.add("menu-item--active");
 			});
 		});
 	}
@@ -48,6 +109,8 @@ const handleTopLevelMenuItemClick = () => {
 const init = () => {
 	handleMenuTriggerClick();
 	handleTopLevelMenuItemClick();
+	addFocusTrapping();
+	allowEscKey();
 };
 
 export default init;
